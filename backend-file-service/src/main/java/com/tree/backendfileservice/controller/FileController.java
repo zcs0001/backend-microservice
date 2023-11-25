60f4e8dcf8d3118f77df0e5999327f3fe8a6583b
thetree <1797978049@qq.com>
2023-11-25 09:24:43 +0800

    微服务

--- backend-file-service/src/main/java/com/tree/backendfileservice/controller/FileController.java
+++ backend-file-service/src/main/java/com/tree/backendfileservice/controller/FileController.java
@@ -8,10 +8,7 @@ import com.tree.backendcommon.exception.BusinessException;
 import com.tree.backendfileservice.service.FileService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
